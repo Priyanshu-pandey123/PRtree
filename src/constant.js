7f4233c0a01db5771af41a6ce6573bca53038f8c
Priyanshu123 <prince678p@gmail.com>
2025-03-04 16:55:49 +0530

    Add the cors origin

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,4 @@
-export const backendUrl = "/api/api/auth";
-export const backendLeadUrl = "/api/api/lead";
+export const backendUrl = "http://43.204.229.77/api/api/auth";
+export const backendLeadUrl = "http://43.204.229.77/api/api/lead";
+// export const backendUrl = "http://localhost:3000/api/auth";
+// export const backendLeadUrl = "http://localhost:3000/api/lead";
