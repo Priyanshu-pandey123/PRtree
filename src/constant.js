3838385925e243e3674b908d045ecc5574a3d6c6
Priyanshu123 <prince678p@gmail.com>
2025-04-01 22:23:04 +0530

    add the prtree domian

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,6 @@
-export const backendUrl = "http://43.204.229.77/api/api/auth";
-export const backendLeadUrl = "http://43.204.229.77/api/api/lead";
+// export const backendUrl = "http://43.204.229.77/api/api/auth";
+// export const backendLeadUrl = "http://43.204.229.77/api/api/lead";
+export const backendUrl = "https://api.theprtree.com/api/auth";
+export const backendLeadUrl = "https://api.theprtree.com/api/lead";
 // export const backendUrl = "http://localhost:3000/api/auth";
 // export const backendLeadUrl = "http://localhost:3000/api/lead";
