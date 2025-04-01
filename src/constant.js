f5f884401a26e4f19906e3b0b7e3553255289408
Priyanshu123 <prince678p@gmail.com>
2025-04-02 01:21:24 +0530

    add the final domain for api

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,6 @@
 // export const backendUrl = "http://43.204.229.77/api/api/auth";
 // export const backendLeadUrl = "http://43.204.229.77/api/api/lead";
-export const backendUrl = "https://theprtree.com/api/auth";
-export const backendLeadUrl = "https://theprtree.com/api/lead";
+export const backendUrl = "https://theprtree.com/api/api/auth";
+export const backendLeadUrl = "https://theprtree.com/api/api/lead";
 // export const backendUrl = "http://localhost:3000/api/auth";
 // export const backendLeadUrl = "http://localhost:3000/api/lead";
